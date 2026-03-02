49daa720435e88ba87ae5cc5bf51a423560acc49
Nikola714 <nikolamp@hiof.no>
2026-03-02 14:02:52 +0100

    localStorage - forelesning 02.03.2026
    Det du skriver til search fil får du lagret i liste og i history i storage -> localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import History from "../components/History"
 
 export default function Home(){
     const [search, setSearch] = useState()
+    const storedHistory = localStorage.getItem("search")
+    const [focused, setFocused] = useState(false)
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+
+    console.log("Denne kommer fra storage", storedHistory)
 
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
 
-    //IKKE GJØR SÅNN, det er sensetive inforrmasjon
-    const apiKey = '90fb6a7'
+    //GJØR SÅNN, slik at ingen ser sensetive inforrmasjon
+    const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    useEffect(() => {
+         localStorage.setItem("search", JSON.stringify(history))
+    }, [history])
 
     const getMovies = async()=>{
         try{
@@ -23,16 +33,28 @@ export default function Home(){
         setSearch(e.target.value)
     }
 
+    const handleSubmit = (e)=> {
+        e.preventDefault()
+        e.target.reset()
+
+        setHistory((prev) => [...prev, search])
+
+       
+    }
+
+    console.log(history)
+
     return (
     <main>
         <h1>Forside</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Søk etter film
-                <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
+                <input type="search" placeholder="Harry Potter" onChange={handleChange} onFocus={() => setFocused(true)} /*onBlur={()=>setFocused(false)}*/></input>
             </label>
+        { focused ? <History history={history} setSearch={setSearch} /> : null } 
+            <button onClick={getMovies}>Søk</button>
         </form>
-        <button onClick={getMovies}>Søk</button>
     </main>
 
     )
