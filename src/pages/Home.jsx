4b4efdc63bddae02e75c342744909779e35745d7
Nikola714 <nikolamp@hiof.no>
2026-03-06 09:53:53 +0100

    Legget til noe komentarer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,32 @@ import { useEffect, useState } from "react"
 import History from "../components/History"
 
 export default function Home(){
+    //search → teksten brukeren skriver
+    //setSearch → funksjon som oppdaterer search
     const [search, setSearch] = useState()
-    const storedHistory = localStorage.getItem("search")
-    const [focused, setFocused] = useState(false)
-    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
 
+    //Henter tidligere søk fra nettleserens lagring
+    //Returnerer string eller null
+    const storedHistory = localStorage.getItem("search")
+
+    //Focused (fokuserer på input) brukes til å vise historikk når input er aktiv.
+    const [focused, setFocused] = useState(false)
+
+    //Hvis localStorage finnes → omgjør til array
+    //Hvis ikke → start med tom array
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
     console.log("Denne kommer fra storage", storedHistory)
 
+    //lager API-url, altså lager url basert av søk
+    //!!!!ER IKKE I BRUKT ENNÅ!!!!!!!
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
 
+    //Henter API-nøkkel fra .env
     //GJØR SÅNN, slik at ingen ser sensetive inforrmasjon
     const apiKey = import.meta.env.VITE_APP_API_KEY
 
+
+    //lagre historikk, når history endres(noen legget noe til) nøkkelord legges i localStorage som text (altså byttes fra array til text og lagrer i localStorage)
     useEffect(() => {
          localStorage.setItem("search", JSON.stringify(history))
     }, [history])
