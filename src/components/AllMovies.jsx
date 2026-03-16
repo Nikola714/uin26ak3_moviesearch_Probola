472fbe6a423e26d7fddbd348a5a8fce18fd3aa25
Nikola714 <nikolamp@hiof.no>
2026-03-16 12:44:34 +0100

    Fikse problemer med usynlig bilder og litt css
    slettet dobbelt årstall, legget til <h3> som manglet, legget til kode som fikser problem med url som fins men viser ikke bilde

--- src/components/AllMovies.jsx
+++ src/components/AllMovies.jsx
@@ -4,8 +4,12 @@ export default function AllMovies({ Title, Year, Type, Poster, noImage}){
 
     return(
         <article className="movies">
-            <Link className="link" to={`movie/:${Title}`}><img className="image" src={Poster !== "N/A" ? Poster : noImage} alt={Title} /></Link>{/*ChatGPT sin kode for å få mitt bilde isteden for bilde som mangler ( https://chatgpt.com/share/69b2c926-6f14-800c-a137-3fdcd708dbb1 )*/}
-            <Link  className="link" to={`movie/:${Title}`}>{Title}</Link> {/*Hjelp fra ChatGpt: ( https://chatgpt.com/share/69b7c533-f4bc-800c-8435-7f41917a6c9e ) */}
+            <Link className="link" to={`movie/:${Title}`}><img className="image" src={Poster !== "N/A" ? Poster : noImage} 
+            onError={(e)=> {e.target.src= noImage}}  // Kode fra Amanda Torstensen til å fikse problem med utl for bilder som kan ikke bli lest
+            alt={Title} />
+            </Link>{/*ChatGPT sin kode for å få mitt bilde isteden for bilde som mangler ( https://chatgpt.com/share/69b2c926-6f14-800c-a137-3fdcd708dbb1 )*/}
+
+            <Link  className="link" to={`movie/:${Title}`}><h3>{Title}</h3></Link> {/*Hjelp fra ChatGpt: ( https://chatgpt.com/share/69b7c533-f4bc-800c-8435-7f41917a6c9e ) */}
             <p>{Year}</p>
             <p>{Type}</p>
         </article>
